d992376732271f27ab850c65f54d470fd0b1d720
Vishvishwanathan <138995986+Vishvishwanathan@users.noreply.github.com>
2024-03-14 14:46:20 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,6 @@ router.get("/api", (req, res) => {
   res.status(200).send(`
     <h1 style="text-align:center">Welcome to Backend of Dress colour suggrstion</h1>`);
 });
-router.use("/", UserRoutes);
+router.use("/api", UserRoutes);
 
 module.exports = router;
